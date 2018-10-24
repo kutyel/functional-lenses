aff447a919976ae23b86837ff9cf843c3d2b7d22
kutyel <flavio.corparios@ext.soprasteria.com>
2018-10-24 10:44:02 +0200

    Add theme 💅🏼

---
+++ components/theme.js
@@ -0,0 +1,13 @@
+import { dark } from 'mdx-deck/themes'
+import codeSurfer from 'prism-react-renderer/themes/nightOwl'
+
+export default {
+  ...dark,
+  codeSurfer,
+  monospace: 'Dank Mono',
+  colors: {
+    text: 'rgb(214, 222, 235)',
+    code: 'rgb(130, 170, 255)',
+    background: 'rgb(1, 22, 39)',
+  },
+}
