64f3575b30dbed66ebdf0a646838cb69cc6ab3c4
kutyel <flavio.corparios@ext.soprasteria.com>
2018-10-26 09:37:04 +0200

    Add lenses! 🕶️

--- components/lenses.js
+++ components/lenses.js
@@ -1,23 +1,23 @@
 // prop :: s -> {s: a} -> a?
-const prop = key => obj => obj[key]
+export const prop = curry((key, obj) => obj[key])
 
 // assoc :: String -> a -> {k: v} -> {k: v}
-const assoc = key => val => obj => ({ ...obj, [key]: val })
+export const assoc = curry((key, val, obj) => ({ ...obj, [key]: val }))
 
 // lens :: (s → a) → ((a, s) → s) → Lens s a
-const lens = (get, set) => ({ get, set })
+export const lens = curry((get, set) => ({ get, set }))
 
 // view :: Lens s a → s → a
-const view = (lens, obj) => lens.get(obj)
+export const view = curry((lens, obj) => lens.get(obj))
 
 // set :: Lens s a → a → s → s
-const set = (lens, val, obj) => lens.set(val, obj)
+export const set = curry((lens, val, obj) => lens.set(val, obj))
 
 // over :: Lens s a → (a → a) → s → s
-const over = (lens, f, obj) => lens.set(f(lens.get(obj)), obj)
+export const over = curry((lens, f, obj) => lens.set(f(lens.get(obj)), obj))
 
 // lensProp :: String → Lens s a
-const lensProp = key => lens(prop(key), assoc(key))
+export const lensProp = key => lens(prop(key), assoc(key))
 
 // lensIndex :: Number → Lens s a
-const lensIndex = index => lens(prop(index), assoc(index))
+export const lensIndex = index => lens(prop(index), assoc(index))
